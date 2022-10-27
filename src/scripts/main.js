2813ed09e157e19d07cd9cf7907cb025853ba781
Ammar Sufyan <63483228+neouranos@users.noreply.github.com>
2022-10-27 10:57:54 +0700

    Add responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,14 @@
 // HAMBURGER MENU ANIMATION
 let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
+  navbar.classList.toggle("open-menu");
   menu.classList.toggle("move");
+};
+window.onscroll = () => {
+  navbar.classList.remove("open-menu");
+  menu.classList.remove("move");
 };
 
 // HEADER CHANGE ON SCROLL //
